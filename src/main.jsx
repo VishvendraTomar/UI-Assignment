394e9bdff4e2ab7401ffc96338c3bafe1cd34118
Vishvendra Tomar <vishvendratomar29@gmail.com>
2024-04-22 02:44:49 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { ChakraProvider } from "@chakra-ui/react";
 import reportWebVitals from './reportWebVitals'; // Import the reportWebVitals function
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,5 @@
 
-import '../App.css'
+// import '../App.css'
 import ChatApp from '../components/ChatApp'
 import Sidebar from '../components/Sidebar'
 
